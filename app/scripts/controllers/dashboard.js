9b5bbe51ef41390b15d48734703e6d21c41aca0f
Vatsalya Goel <vatsalyagoel@gmail.com>
2016-07-31 03:11:24 +1000

    Add Get real data from firebase

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -2,15 +2,16 @@
  * Created by vatsalyagoel on 30/07/2016.
  */
 angular.module('dequeApp')
-  .controller('DashboardCtrl', ["$scope","$firebaseAuth", "$firebaseArray", function ($scope, $firebaseAuth, $firebaseArray) {
-      var queueItemsRef = firebase.database().ref().child("queueitems");
-      var user = $firebaseAuth().$getAuth();
-      var query = queueItemsRef.orderByChild("userid").equalTo(user.uid);
-      $scope.queueItems = $firebaseArray(query);
-      $scope.queueItems.$loaded().then(function() {
-            console.log($scope.queueItems.$value); // "bar"
-          });
-
+  .controller('DashboardCtrl', ["$scope","$firebaseAuth", "$firebaseArray", "$timeout", function ($scope, $firebaseAuth, $firebaseArray, $timeout) {
+      $scope.tickInterval = 30000;
+      var tick = function() {
+        var queueItemsRef = firebase.database().ref().child("queueitems");
+        var user = $firebaseAuth().$getAuth();
+        var query = queueItemsRef.orderByChild("userid").equalTo(user.uid);
+        $scope.queueItems = $firebaseArray(query);
+        $timeout(tick,  $scope.tickInterval);
+      }
+      tick();
     }
   ]);
 
---
+++ app/scripts/controllers/duration.js
@@ -0,0 +1,34 @@
+/**
+ * Created by vatsalyagoel on 31/07/2016.
+ */
+
+angular.module('dequeApp')
+  .controller('DurationCtrl', function ($scope, $timeout) {
+    $scope.duration  = "";
+    $scope.tickInterval = 1000;
+
+    $scope.init = function (startTime, endTime) {
+      $scope.endTime = endTime;
+      $timeout(tick, $scope.tickInterval);
+    };
+
+    var str_pad_left = function(string,pad,length) {
+      return (new Array(length+1).join(pad)+string).slice(-length);
+    };
+
+    var tick = function() {
+      var duration = Math.floor(($scope.endTime - Date.now())/1000);
+      if(duration < 0 )
+      {
+        $scope.duration = "00:00"
+      }
+      else
+      {
+        var minutes = Math.floor(duration / 60);
+        var seconds = duration % 60;
+        $scope.duration = str_pad_left(minutes,'0',2)+':'+str_pad_left(seconds,'0',2);
+        $timeout(tick, $scope.tickInterval);
+      }
+    }
+
+  });
