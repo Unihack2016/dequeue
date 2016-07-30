ec5b8a25642a8496d499c78b04ba40359b758a34
AWiyono <arvinwiyono@gmail.com>
2016-07-31 09:23:42 +1000

    QR code url updated

--- app/scripts/controllers/qrgenerate.js
+++ app/scripts/controllers/qrgenerate.js
@@ -7,7 +7,7 @@ angular.module('dequeApp')
   .controller('QRCtrl', function($scope, $http){
 
     var getTinyAndQR = function(){
-      var url = "http://api.qrserver.com/v1/create-qr-code/?data=http://10.25.129.250:9001/%23/" + $scope.key + "&size=150x150";
+      var url = "http://api.qrserver.com/v1/create-qr-code/?data=http://10.25.129.250:9001/%23/request_item/" + $scope.key + "&size=150x150";
       $scope.QRUrl = url;
     }
 
