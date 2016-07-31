0f4b13f9ebb8193c68365e22f40842f7c6acf2e7
vc <vcgamesii@gmail.com>
2016-07-31 10:51:25 +1000

    QR code generator now uses internet connected deque-3820e.firebaseapp.com domain

--- app/scripts/controllers/qrgenerate.js
+++ app/scripts/controllers/qrgenerate.js
@@ -7,9 +7,9 @@ angular.module('dequeApp')
   .controller('QRCtrl', function($scope, $http){
 
     var getTinyAndQR = function(){
-      var url = "http://api.qrserver.com/v1/create-qr-code/?data=http://10.25.129.250:9001/%23/request_item/" + $scope.key + "&size=150x150";
+      var url = "http://api.qrserver.com/v1/create-qr-code/?data=https://deque-3820e.firebaseapp.com/%23/request_item/" + $scope.key + "&size=150x150";
       $scope.QRUrl = url;
-    }
+    };
 
     $scope.init = function(key){
       $scope.key = key;
